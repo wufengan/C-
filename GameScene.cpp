b35c690a22d2b42722444d826893099d54777f48
hefeng1996 <245998875@qq.com>
2017-12-29 11:03:11 +0800

    FishingJoy

--- GameScene.cpp
+++ GameScene.cpp
@@ -1,5 +1,5 @@
 #include "GameScene.h"
-
+//#include "FishJoyData.h"
 GameScene::GameScene()
 {
 }
@@ -12,15 +12,26 @@ bool GameScene::init()
 		preloadResources();
 		//因为~GameScene()中需要CC_SAFE_RELEASE(_menuLayer)， 如果其它层创建失败，_menuLayer将不创建，
 		//所以_menuLayer要先于其他层创建， 否则将报 "reference count greater than 0" 错误
-		_menuLayer = MenuLayer::create(); 
-		CC_BREAK_IF(!_menuLayer);
-		CC_SAFE_RETAIN(_menuLayer); 
-		_backgroundLayer = BackgroundLayer::create();
-		CC_BREAK_IF(!_backgroundLayer);
-		addChild(_backgroundLayer);
-		_fishLayer = FishLayer::create();
-		CC_BREAK_IF(!_fishLayer);
-		addChild(_fishLayer);
+		menuLayer = MenuLayer::create(); 
+		CC_BREAK_IF(!menuLayer);
+		CC_SAFE_RETAIN(menuLayer); 
+		backgroundLayer = BackgroundLayer::create();
+		CC_BREAK_IF(!backgroundLayer);
+		this->addChild(backgroundLayer);
+		fishLayer = FishLayer::create();
+		CC_BREAK_IF(!fishLayer);
+		this->addChild(fishLayer);
+		cannonLayer = CannonLayer::create();
+		CC_BREAK_IF(!cannonLayer);
+		this->addChild(cannonLayer);
+		touchLayer = TouchLayer::create();
+		CC_BREAK_IF(!touchLayer);
+		this->addChild(touchLayer);
+		paneLayer = PanelLayer::create();
+		CC_BREAK_IF(!paneLayer);
+		this->addChild(paneLayer);
+		paneLayer->getGoldCounter()->setNumber(FishJoyData::sharedFishingJoyData()->getGold());
+		this->scheduleUpdate();
 		return true;
 	} while (0);
 	return false;
@@ -28,6 +39,7 @@ bool GameScene::init()
 
 void GameScene::preloadResources(void)
 {
+	PersonalAudioEngine::sharePersonalAudioEngine();
 	CCSpriteFrameCache* spriteFrameCache = CCSpriteFrameCache::sharedSpriteFrameCache();
 	//修改以下plist文件， 删除key中的中文， 否则spriteFrameByName函数无法找到Frame，将返回NULL
 	spriteFrameCache->addSpriteFramesWithFile("FishActor-Large-ipadhd.plist");		//修改metadata->realTextureFileName->FishActor-Large-ipadhdhd.png, textureFileName->FishActor-Large-ipadhd.png
@@ -35,6 +47,13 @@ void GameScene::preloadResources(void)
 	spriteFrameCache->addSpriteFramesWithFile("FishActor-Shark-ipadhd.plist");		//同上
 	spriteFrameCache->addSpriteFramesWithFile("FishActor-Small-ipadhd.plist");		//同上
 	spriteFrameCache->addSpriteFramesWithFile("FishActor-Mid-ipadhd.plist");			//同上
+	spriteFrameCache->addSpriteFramesWithFile("cannon-ipadhd.plist");
+	spriteFrameCache->addSpriteFramesWithFile("Item-chaojiwuqi-ipadhd.plist");
+
+	CCTextureCache *textureCache = CCTextureCache::sharedTextureCache();
+	textureCache->addImage("ui_button_63-ipadhd.png");
+	textureCache->addImage("ui_button_65-ipadhd.png");
+
 	char str[][50] = { "SmallFish", "Croaker", "AngelFish", "Amphiprion", "PufferS", 
 		"Bream", "Porgy", "Chelonian", "Lantern", "Ray", "Shark", "GoldenTrout", "GShark", 
 		"GMarlinsFish", "GrouperFish", "JadePerch", "MarlinsFish", "PufferB" };
@@ -61,5 +80,104 @@ void GameScene::preloadResources(void)
 
 GameScene::~GameScene()
 {
-	CC_SAFE_RELEASE(_menuLayer);
+	CC_SAFE_RELEASE(menuLayer);
+}
+
+void GameScene::cannonAimAt(CCPoint target)
+{
+	cannonLayer->aimAt(target);
+}
+
+void GameScene::cannonShootTo(CCPoint target)
+{
+	int cost = cannonLayer->getWeapon()->getCannonType() + 1;
+	if (FishJoyData::sharedFishingJoyData()->getGold() >= cost)
+	{
+		cannonLayer->shootTo(target);
+		alterGold(-cost);
+	}
+
+	//_cannonLayer->shootTo(target);
+}
+
+bool GameScene::checkOutCollisionBetweenFishesAndBullet(Bullet* bullet)
+{
+	CCPoint bulletPos = bullet->getCollosionPoint();
+	CCArray* fishArray = fishLayer->getFishArray();
+	CCObject* obj = NULL;
+	CCARRAY_FOREACH(fishArray, obj)
+	{
+		Fish* fish =(Fish*)obj;
+		if(fish->isRunning() && fish->getCollisionArea().containsPoint(bulletPos))
+		{
+			bullet->end();
+			return true;
+		}
+	}
+	return false;
+}
+
+void GameScene::checkOutCollision()
+{
+	CCArray* bullets = cannonLayer->getWeapon()->getBullets();
+	CCObject* obj = NULL;
+	CCARRAY_FOREACH(bullets, obj)
+	{
+		Bullet* bullet = (Bullet*)obj;
+		if(bullet->isVisible())
+		{
+			if(checkOutCollisionBetweenFishesAndBullet(bullet))
+			{
+				checkOutCollisionBetweenFishesAndFishingNet(bullet);
+			}
+		}
+	}	
+}
+
+void GameScene::update(float delta)
+{
+	checkOutCollision();
+}
+
+void GameScene::fishWillBeCaught(Fish* fish)
+{
+	float weaponPercents[k_Cannon_Count] = { 0.3, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1 };
+	float fishPercents[	k_Fish_Type_Count] = { 1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4 };
+	int _cannonType = cannonLayer->getWeapon()->getCannonType();
+	int _fishType = fish->getType();
+	float percentage =(float)_cannonType * _fishType;
+	if(CCRANDOM_0_1() < percentage)//1.1
+	{
+		fish->beCaught();
+		int reward = STATIC_DATA_INT(CCString::createWithFormat(STATIC_DATA_STRING("reward_format"),_fishType)->getCString());
+		alterGold(reward);
+	}
+	
+}
+
+void GameScene::checkOutCollisionBetweenFishesAndFishingNet(Bullet* bullet)
+{
+	Weapon* weapon = cannonLayer->getWeapon();
+	CCRect rect = weapon->getCollisionArea(bullet);
+	CCArray* fishArray = fishLayer->getFishArray();
+	CCObject* obj = NULL;
+	CCARRAY_FOREACH(fishArray, obj)
+	{
+		Fish* fish = (Fish*)obj;
+		if(fish->isRunning() && rect.intersectsRect(fish->getCollisionArea()))
+		{
+			fishWillBeCaught(fish);
+		}
+	}
+}
+void GameScene::alterGold(int delta)
+{
+	FishJoyData* fishJoyData = FishJoyData::sharedFishingJoyData();
+	fishJoyData->alterGold(delta);
+	paneLayer->getGoldCounter()->setNumber(fishJoyData->getGold());
+}
+void GameScene::onEnter()
+{
+	CCScene::onEnter();
+	PersonalAudioEngine::sharePersonalAudioEngine()->playBackgroundMusic(3);
 }
--- GameScene.h
+++ GameScene.h
@@ -3,6 +3,13 @@
 #include "BackgroundLayer.h"
 #include "FishLayer.h"
 #include "MenuLayer.h"
+#include "CannonLayer.h"
+#include "TouchLayer.h"
+#include "Fish.h"
+#include "PanelLayer.h"
+#include "FishJoyData.h"
+#include "GoldCounterLayer.h"
+#include "PersonalAudioEngine.h"
 USING_NS_CC;
 
 class GameScene :
@@ -13,10 +20,25 @@ public:
 	CREATE_FUNC(GameScene)
 	virtual bool init();
 	virtual ~GameScene();
+	void cannonAimAt(CCPoint target);
+	void cannonShootTo(CCPoint target);
+	void alterGold(int delta);
+	void onEnter();
 protected:
-	BackgroundLayer* _backgroundLayer;
-	FishLayer* _fishLayer;
-	MenuLayer* _menuLayer;
+	BackgroundLayer* backgroundLayer;
+	FishLayer* fishLayer;
+	MenuLayer* menuLayer;
+	CannonLayer* cannonLayer;
+	TouchLayer* touchLayer;
+	PanelLayer* paneLayer;
+
+	//GoldCounterLayer* alterGold;
+
 	void preloadResources(void);
+	bool checkOutCollisionBetweenFishesAndBullet(Bullet* bullet);
+	void checkOutCollision();
+	virtual void update(float delta);
+	void fishWillBeCaught(Fish* fish);
+	void checkOutCollisionBetweenFishesAndFishingNet(Bullet* bulet);
 };
 
